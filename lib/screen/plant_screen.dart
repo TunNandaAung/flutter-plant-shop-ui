f3cb2943aada6ba6bcc544256bd1afce4df64eff
TunNandaAung <tunnandaaung@outlook.com>
2019-11-24 23:33:12 +0630

    Implement UI

---
+++ lib/screen/plant_screen.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:plant_shop_ui/model/model.dart';
+
+class PlantScreen extends StatefulWidget {
+  final Plant plant;
+
+  PlantScreen({Key key, this.plant}) : super(key: key);
+
+  @override
+  _PlantScreenState createState() => _PlantScreenState();
+}
+
+class _PlantScreenState extends State<PlantScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  Container(
+                    padding:
+                        EdgeInsets.only(left: 30.0, right: 30.0, top: 40.0),
+                    height: 520.0,
+                    color: Color(0xFF32A060),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: Icon(Icons.arrow_back_ios,
+                                  size: 30, color: Colors.white),
+                            ),
+                            Icon(Icons.shopping_cart,
+                                size: 30, color: Colors.black)
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        Text(
+                          widget.plant.category.toUpperCase(),
+                          style: TextStyle(color: Colors.white, fontSize: 15.0),
+                        ),
+                        SizedBox(height: 5.0),
+                        Text(
+                          widget.plant.name,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30.0,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 40.0,
+                        ),
+                        Text(
+                          'FROM',
+                          style: TextStyle(color: Colors.white, fontSize: 15.0),
+                        ),
+                        SizedBox(height: 5.0),
+                        Text(
+                          '\$${widget.plant.price}',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25.0,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 40.0,
+                        ),
+                        Text(
+                          'SIZE',
+                          style: TextStyle(color: Colors.white, fontSize: 15.0),
+                        ),
+                        SizedBox(height: 5.0),
+                        Text(
+                          widget.plant.size,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30.0,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 40.0,
+                        ),
+                        RawMaterialButton(
+                          padding: EdgeInsets.all(20.0),
+                          shape: CircleBorder(),
+                          elevation: 2.0,
+                          fillColor: Colors.black,
+                          child: Icon(Icons.add_shopping_cart,
+                              color: Colors.white, size: 35.0),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    right: 20.0,
+                    bottom: 30.0,
+                    child: Hero(
+                      tag: widget.plant.imageUrl,
+                      child: Image(
+                        height: 280.0,
+                        width: 280.0,
+                        image: AssetImage(widget.plant.imageUrl),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                height: 400.0,
+                transform: Matrix4.translationValues(0.0, -20.0, 0.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20.0)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding:
+                          EdgeInsets.only(left: 30.0, right: 30.0, top: 40.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'All to know...',
+                            style: TextStyle(
+                                fontSize: 24.0, fontWeight: FontWeight.w600),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(widget.plant.description,
+                              style: TextStyle(color: Colors.black87))
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 30.0, vertical: 40.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Details',
+                            style: TextStyle(
+                                fontSize: 24.0, fontWeight: FontWeight.w600),
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            'Plant height: 35-45cm',
+                            style: TextStyle(
+                              color: Colors.black87,
+                              fontSize: 24.0,
+                            ),
+                          ),
+                          Text(
+                            'Nusery pot width: 12cm',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 16.0),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
